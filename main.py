e5bba2f1df125e3001c2d431b2a6f5388e0c44bc
chris karvouniaris <christos.karvouniaris@lightsourcelabs.com>
2022-11-22 15:51:50 +0200

    moved to port 9999

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ import eventlet
 from eventlet import wsgi
 
 if __name__ == '__main__':
-    # app.secret_key = os.urandom(1)
-    # wsgi.server(eventlet.listen(('0.0.0.0', 5000)), app)
-    app.run(debug=True)
+    app.secret_key = os.urandom(1)
+    wsgi.server(eventlet.listen(('0.0.0.0', 9999)), app)
+    # app.run(debug=True)
